66ce51effb77a9316e596181b593af5f19648aaa
leonel98 <lalvarezb1@unicartagena.edu.co>
2026-05-22 22:30:00 -0500

    cambios de la rama utils

---
+++ src/main/java/utils/CalculadoraDeducciones.java
@@ -0,0 +1,19 @@
+package utils;
+public class CalculadoraDeducciones {
+
+    private static final double SEGURIDAD_SOCIAL = 0.04;
+    private static final double ARL = 0.01;
+
+    public static double calcularSaludYPension(
+            double salarioBruto) {
+
+        return salarioBruto * SEGURIDAD_SOCIAL;
+    }
+
+    public static double calcularARL(
+            double salarioBruto) {
+
+        return salarioBruto * ARL;
+    }
+
+}
